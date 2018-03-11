6c347095105542d74dd059902c6d8f58e4cfbc1a
= <=>
2018-03-11 21:51:20 +0800

    Add redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from './actions/actionCreators';
+import Home from './containers/Home';
+import NavBarComponent from './components/NavBarComponent';
 
-export default class App extends React.Component {
-    render(){
-        return(
-            <Router>
-                <Route path="/" component={Home}/>
-            </Router>
-        );
+function mapStateToProps (state) {
+    return {
+        isLoggedIn: state.isLoggedIn,
+        authToken: state.authToken,
+        machines: state.machines,
+        brands: state.brands
     }
 }
+
+function mapDispatchToProps (dispatch){
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps);
+
+export const NavBar = App(NavBarComponent);
+export default App(Home);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import authToken from './authToken';
+import isLoggedIn from './isLoggedIn';
+import machines from './machines';
+import brands from './brands';
 
-const rootReducer = combineReducers({authToken, routing: routerReducer});
+const rootReducer = combineReducers({authToken, brands, isLoggedIn, machines, routing: routerReducer});
 
 export default rootReducer;
