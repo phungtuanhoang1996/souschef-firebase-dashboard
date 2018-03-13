e62c49719b477ac8617a2b8bd185bd9090bcfe0b
Dansyuqri <justanormalme@gmail.com>
2018-03-13 17:43:12 +0800

    Add sidebar drawer

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import firebase  from '../firebase';
+import DrawerNavComponent from '../components/drawer/DrawerNavComponent';
+import OverviewComponent from '../components/overview/OverviewComponent';
+import { Col, Container, Row } from 'reactstrap';
+import './Dashboard.css';
 
 export default class Dashboard extends React.Component {
     render() {
         return (
-            <div>
-                test
+            <div className="wrapper">
+                <div className="sidebar">
+                    <DrawerNavComponent currentUser={this.props.currentUser} currentBrandName={this.props.currentBrandName}/>
+                </div>
+                <div className="main">
+                    <OverviewComponent/>
+                </div>
             </div>
         );
     }
